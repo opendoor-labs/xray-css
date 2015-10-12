827b7ac4f026cb442ca275f99454a17bbb63a36e
Visnu Pitiyanuvath <visnupx@gmail.com>
2015-10-11 17:47:09 -0700

    refactoring

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ function *addSuite(parent, file) {
   const suite = Mocha.Suite.create(parent, basename);
   const nightmare = Nightmare();
 
-  suite.afterAll(nightmare.end.bind(nightmare));
-
   // load the file
   yield nightmare
     .goto(`file://${filepath}`)
@@ -46,28 +44,19 @@ function *addSuite(parent, file) {
     .exists('body');
 
   // pull out describe blocks
-  var describes = yield nightmare.evaluate(function() {
-    var map = Array.prototype.map;
-    return map.call(document.querySelectorAll('describe'), function(node) {
-      return {
-        title: node.title,
-        x: node.offsetLeft,
-        y: node.offsetTop,
-        width: node.offsetWidth,
-        height: node.offsetHeight
-      };
-    });
-  });
+  var describes = yield nightmare.evaluate(getDescribes);
 
-  if (describes.length === 0)
+  if (describes.length)
+    suite.afterAll(nightmare.end.bind(nightmare));
+  else
     yield nightmare.end();
 
+  const latestPath = path.join(dirname, 'latest', basename),
+        passingPath = path.join(dirname, 'passing', basename),
+        diffPath = path.join(dirname, 'diff', basename);
   // add a test for each describe
   describes.forEach(function(describe, i) {
     suite.addTest(new Mocha.Test(describe.title, function(done) {
-      const latestPath = path.join(dirname, 'latest', basename),
-            passingPath = path.join(dirname, 'passing', basename),
-            diffPath = path.join(dirname, 'diff', basename);
       const title = describe.title || i,
             latest = path.join(latestPath, title) + '.png',
             passing = path.join(passingPath, title) + '.png',
@@ -78,18 +67,19 @@ function *addSuite(parent, file) {
         mkdirp(passingPath),
         mkdirp(diffPath)
       ],
-      function *() { yield nightmare.screenshot(latest, describe) },
+      function *() { yield nightmare.screenshot(latest, describe.clip) },
       function(err, next) {
         if (err) return next(err);
         if (fs.existsSync(passing)) {
           const blink = new BlinkDiff({
             imageAPath: latest,
             imageBPath: passing,
-            imageOutputPath: diff
+            imageOutputPath: diff,
+            threshold: 20,
           });
           blink.run(function(err, result) {
             if (err) return next(err);
-            next(blink.hasPassed(result.code) ? null : Error('Output do not match'));
+            next(blink.hasPassed(result.code) ? null : Error('Output does not match'));
           });
         } else {
           fs.createReadStream(from).pipe(fs.createWriteStream(to))
@@ -97,5 +87,20 @@ function *addSuite(parent, file) {
         }
       })(done);
     }));
+  });
+}
+
+function getDescribes() {
+  var map = Array.prototype.map;
+  return map.call(document.querySelectorAll('describe'), function(node) {
+    return {
+      title: node.title,
+      clip: {
+        x: node.offsetLeft,
+        y: node.offsetTop,
+        width: node.offsetWidth,
+        height: node.offsetHeight,
+      }
+    };
   });
 }
